e25e9ad39a80b16083e7f3da1146b14054e0e891
shubhammore1310 <98207056+shubhammore1310@users.noreply.github.com>
2023-11-05 20:05:11 +0530

    Update nav.jsx
    added route to about us component. And added scroll down animation for contact us button.

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,52 +1,82 @@
-import React from "react";
-function Nav(){
-    var [position,changeposition]=React.useState(0);
-    var [opac,changeopac]=React.useState(0);
-    function handledot(event){
-        document.querySelector(".dot").style.opacity=1;
-        
-        var name=event.target.name;
-        if(name==="one"){
-            changeposition(30);
-            changeopac(1);
-        }
-        if(name==="two"){
-            changeposition(125);
-            changeopac(1);
-        }
-        if(name==="three"){
-            changeposition(240);
-            changeopac(1);
-        }
+import React, { useState, useEffect } from "react";
 
+function Nav() {
+  const [position, changeposition] = useState(0);
+  const [opac, changeopac] = useState(0);
 
+  const handleScrollToFooter = () => {
+    const footer = document.getElementById("footer");
+    if (footer) {
+      const footerPosition = footer.getBoundingClientRect().top + window.scrollY;
+      window.scrollTo({
+        top: footerPosition,
+        behavior: "smooth",
+      });
     }
-    function opacChange(){
-        changeposition(0);
-        changeopac(0);
-    }
-    return(
-        <div className="main-nav">
-            <div className="logo">
-            <div className="logo-img">
-                   <img src="./images/logo.png" alt="" />
-                </div>
-                <div className="name">
-                    <p>ShareFood.Com</p>
-                </div>
-              
-            </div>
-            <div className="options">
-                <a onMouseOver={handledot} onMouseOut={opacChange} name="one" href="/">Home</a>               
-                <a onMouseOver={handledot} onMouseOut={opacChange} name="two" href="#footer">Contact</a>
-                <a onMouseOver={handledot} onMouseOut={opacChange} name="three">About us</a>
-                <div className="line">
-                     <div className="dot" style={{left:position,opacity:opac}}>
+  };
 
-                     </div>
-                </div>
-            </div>
+  const handledot = (event) => {
+    document.querySelector(".dot").style.opacity = 1;
+
+    const name = event.target.name;
+    if (name === "one") {
+      changeposition(30);
+      changeopac(1);
+    }
+    if (name === "two") {
+      changeposition(125);
+      changeopac(1);
+    }
+    if (name === "three") {
+      changeposition(240);
+      changeopac(1);
+    }
+  };
+
+  const opacChange = () => {
+    changeposition(0);
+    changeopac(0);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", opacChange);
+    return () => {
+      window.removeEventListener("scroll", opacChange);
+    };
+  }, []);
+
+  return (
+    <div className="main-nav">
+      <div className="logo">
+        <div className="logo-img">
+          <img src="./images/logo.png" alt="" />
         </div>
-    )
+        <div className="name">
+          <p>ShareFood.Com</p>
+        </div>
+      </div>
+      <div className="options">
+        <a onMouseOver={handledot} onMouseOut={opacChange} name="one" href="/">
+          Home
+        </a>
+        <a
+          onMouseOver={handledot}
+          onMouseOut={opacChange}
+          name="two"
+          href="#"
+          onClick={handleScrollToFooter}
+        >
+          Contact
+        </a>
+        <a onMouseOver={handledot} onMouseOut={opacChange} name="three" href="AboutUs">
+          About us
+        </a>
+        <div className="line">
+          <div className="dot" style={{ left: position, opacity: opac }}></div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default Nav;
