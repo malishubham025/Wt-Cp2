bf408bf89da16f61b23a42880187dba1ca8514a2
shubhammore1310 <98207056+shubhammore1310@users.noreply.github.com>
2023-11-05 19:59:21 +0530

    Update index.js
    Added about component

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import Login from './components/login-signup';
 import AdminLogin from './components/Admin';
 import AdminPage from './components/mainAdmin';
 import Volunteers from './components/Volunteers';
+import AboutUs from './components/AboutUs';
 import { BrowserRouter as Router ,Routes,Route } from 'react-router-dom';
 ReactDOM.render(
   <Router>
     <Routes>
        <Route path="/" element={<App/>}/>
        <Route path="/login" element={<Login/>}/>
+       <Route path="/AboutUs" element={<AboutUs/>} />
        <Route path="/Admin" element={<AdminLogin/>}/>
        <Route path="/admin-page" element={<AdminPage/>}/>
        <Route path="/volunteers" element={<Volunteers/>}/>
