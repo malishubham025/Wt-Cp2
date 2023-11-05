d49ece68f1c8a89944908f66029bfb048a939f64
shubhammore1310 <98207056+shubhammore1310@users.noreply.github.com>
2023-11-05 20:00:58 +0530

    Update App.jsx
    changes app, moved projects and volunteers to about component.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,27 +2,19 @@ import React from "react";
 import Nav from './nav';
 import Footer from './footer';
 import Main from './main';
-import Projects from './projects';
-import Volunteer from './volunteer';
 import Donate from './donate';
-import Reply from './reply';
 import Form from './form';
 import Technical from './technical';
 function App(){
     return(
-        <div>
-                
-   <Nav/>
-   <Main/>
-   <Form />
-	 <Technical />
-   <Projects/>
-   <Donate />
-   <Volunteer />
-   <Reply />
-   <Footer />
-   
-  </div>
+        <div>    
+            <Nav/>
+            <Main/>
+            <Form />
+            <Technical />
+            <Donate />
+            <Footer />
+        </div>
     );
 }
 export default App;
